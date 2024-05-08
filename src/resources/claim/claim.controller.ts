22e62cf63ec269d1ce3c85acf19a7cc261dcefb7
Henok-Matheas <henokmatheaskebede@gmail.com>
2024-05-08 12:25:53 +0300

    feat: Implement claim reminders-expiration endpoint

--- src/resources/claim/claim.controller.ts
+++ src/resources/claim/claim.controller.ts
@@ -5,6 +5,8 @@ import { handleErrorResponse } from "../../helpers/errorHandle";
 import { Report, ReportStatus } from "../report/report.model";
 import { updateReportStatus } from "../report/report.repository";
 import { Claim, calculateDueDate } from "./claim.model";
+import { deleteClaim } from "./claim.repository";
+import { deleteClaimAndNotify } from "./claim.service";
 
 export async function createClaims(
     req: Request,
@@ -61,7 +63,7 @@ export async function getClaim(
     }
 }
 
-export async function deleteClaim(
+export async function removeClaim(
     req: Request,
     res: Response,
     next: NextFunction
@@ -69,12 +71,7 @@ export async function deleteClaim(
     try {
         const claimer_id = res.locals.user._id;
 
-        const claim = await Claim.findByIdAndDelete(claimer_id);
-
-        if (!claim)
-            throw new NotFoundError("There is no claim made by this user");
-
-        const updatedReport = await updateReportStatus({ report_id: claim.report, status: ReportStatus.Open });
+        const claim = deleteClaim({ _id: claimer_id });
 
         return new SuccessMsgResponse("Claim deleted successfully").send(res);
 
@@ -83,3 +80,30 @@ export async function deleteClaim(
     }
 }
 
+
+export async function claimActions(
+    req: Request,
+    res: Response,
+    next: NextFunction
+) {
+    try {
+        const claims = await Claim.find({}, { _id: 1, due_date: 1, report: 1 });
+        const tasks = [];
+        const currentDate = new Date();
+
+        claims.forEach(claim => {
+            if (currentDate >= claim.due_date) {
+                tasks.push(deleteClaimAndNotify({ _id: claim._id, report_id: claim.report }));
+            } else if (currentDate < claim.due_date) {
+                // tasks.push(console.log("Send reminders to claimer"));
+            }
+        })
+
+        await Promise.all(tasks);
+
+        return new SuccessMsgResponse("Claim Actions performed successfully").send(res);
+
+    } catch (error) {
+        handleErrorResponse(error, res);
+    }
+}
