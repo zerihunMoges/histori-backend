7582aced123361044e208100e31e58c0c576a5b0
Henok-Matheas <henokmatheaskebede@gmail.com>
2024-05-23 12:29:34 +0300

    feat: implement submit history review endpoint

--- src/resources/review/review.service.ts
+++ src/resources/review/review.service.ts
@@ -1,9 +1,9 @@
 import { InternalError } from "../../core/ApiError";
 import { deleteUserReviewRepo } from "./review.repository";
 
-export async function deleteReviewAndNotify({ _id, report_id }) {
+export async function deleteReviewAndNotify({ _id, report_id, reviewer_id }) {
     try {
-        const review = await deleteUserReviewRepo(_id);
+        const review = await deleteUserReviewRepo({ _id, reviewer_id });
 
         // Send notification to the user
 
