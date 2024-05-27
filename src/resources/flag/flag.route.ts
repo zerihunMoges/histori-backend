50fdd4a89e0337c1340d22bde113e906cb416611
Henok-Matheas <henokmatheaskebede@gmail.com>
2024-05-27 12:51:55 +0300

    feat: implement flag endpoint without returning random flags.

--- src/resources/flag/flag.route.ts
+++ src/resources/flag/flag.route.ts
@@ -1,13 +1,12 @@
 import { Router } from "express";
-import { authenticate, permit } from "../../middlewares/authentication";
-import { Role } from "../../utils/roles";
+import { authenticate } from "../../middlewares/authentication";
 import FlagController from "./flag.controller";
 
 const flagRouter = Router();
 
 flagRouter
     .route("/")
-    .post(authenticate, permit(Role.Admin, Role.Contributor), FlagController.createFlag)
+    .post(FlagController.createFlag)
     .get(authenticate, FlagController.getFlagsByCount);
 
 export default flagRouter;
